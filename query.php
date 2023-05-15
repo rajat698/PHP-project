b53e74382a32489fd4d237d486585c00e54b57c1
rajat698 <67538489+rajat698@users.noreply.github.com>
2023-05-15 18:28:09 -0400

    Query.php form beautified

--- query.php
+++ query.php
@@ -1,7 +1,46 @@
-<form action = "query.php" method = "post">
-   City: <input type = "text" name = "city", value = "city">
-   <input type = "submit">
-    </form>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Utah City Data</title>
+  <style>
+    form {
+      width: 400px;
+      margin: 0 auto;
+      padding: 20px;
+      border: 2px solid #ccc;
+      border-radius: 10px;
+    }
+    label {
+      display: block;
+      margin-bottom: 10px;
+    }
+    input[type="text"] {
+      width: 90%;
+      padding: 10px;
+      border: 2px solid #ccc;
+      border-radius: 5px;
+    }
+    input[type="submit"] {
+      display: block;
+      margin-top: 20px;
+      padding: 10px;
+      background-color: #4CAF50;
+      color: white;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+    }
+  </style>
+</head>
+<body>
+  <form method="post" action="query.php">
+    <label for="text">City</label>
+    <input type="text" id="city" name="city" required>
+    <input type="submit" value="Submit">
+  </form>
+</body>
+</html>
+
 
 <?php
 
